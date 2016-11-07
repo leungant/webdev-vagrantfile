bbd9f1c6b436bce798bab8852b0cb742de9531f2
AL <anthony.leung@gmail.com>
2016-11-07 03:39:14 +0000

    add setup file for ocdaction project

---
+++ wh/setup-ocdaction.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+cd /home/vagrant/files/
+git clone https://github.com/womenhackfornonprofits/ocdaction
+cd frontend
+sudo gem install sass
+npm install  # --no-bin-links
+grunt default &
+cd ..
+sudo pip install -r requirements.txt
+sudo -u postgres createdb ocdaction 
+python ocdaction/manage.py migrate
+cd ocdaction
+echo "Run server with: python manage.py runserver"
+
+
